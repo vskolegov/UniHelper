8c324fea595df8ddcbdf482e566f763098fb3d4e
Александр Горбунов <santerr80@mail.ru>
2023-12-15 21:33:25 +0700

    Update UniHelperBot.py

--- UniHelperBot.py
+++ UniHelperBot.py
@@ -19,13 +19,13 @@ def main(message):
 @bot.message_handler(content_types=["voice"])
 def get_voice(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Напечатать сообщение в виде текста", callback_data="convert"))
+    markup.add(types.InlineKeyboardButton("Напечатать сообщение в виде текста", callback_data="convert to text"))
     bot.send_message(message.chat.id, 'Выберите действие', reply_markup=markup)
     
 @bot.message_handler(content_types=["text"])
 def get_text(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Озвучить текст", callback_data="convert"))
+    markup.add(types.InlineKeyboardButton("Озвучить текст", callback_data="convert to voice"))
     bot.reply_to(message, 'Выберите действие', reply_markup=markup)
 
 bot.polling(none_stop=True)
